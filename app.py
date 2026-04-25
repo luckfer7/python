12e772e8691ac0b7d407de05bab93d6ded9abb03
Lucas Ferreira <lucasferreira059@gmail.com>
2026-04-25 19:07:58 -0300

    loops

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # Ask user for their name
 name = input("What's your name: ")
+age = input("How old are you?: ")
 
 # Say hello to user
-print("Hello, " + name)
-print(name)
+print("Hello, " + name + "You are " + age + "years old")
+if age > 18:
+    print("{name} é maior de idade")
+
+
+#
---
+++ datatypes/datatypes.py
@@ -0,0 +1,62 @@
+# name = "lucas"
+# age = 27
+
+# print(name)
+# print(age)
+
+# #Espaços definem blocos de código
+
+# if age >= 18:
+#     print(name + "é maior de idade")
+
+
+#STRINGS
+
+nome = "Rosineide"
+mensagem = "Olá, tudo bem?"
+
+print(nome.upper())
+print(len(nome)) #conta o numero de caracteres0
+
+
+#INTEIROS
+
+idade = 28
+year = 2026
+
+
+#DECIMAIS ou FLOAT
+
+altura = 1.91
+preco = 19.99
+
+#BOOLEANO
+
+ativo = True
+pago = False
+
+
+#ARRAYS
+
+dadosDoLucas = ["Lucas", 27, True]
+
+
+#DICIONÁRIO (OBJETOS TIPO JSON)
+
+pessoa = {
+    "name" : "lucas",
+    "idade" : 27,
+    "ativo" : True
+}
+
+print(pessoa["idade"])
+
+
+#CONVERSÃO DE TIPOS
+
+idade = "27"
+
+idadeToInt = int(idade)
+print(idade)
+
+#parei no estrutura básica
