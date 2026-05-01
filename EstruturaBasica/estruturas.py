04fdfb3e088d0139fc4783585c411f949fb2185b
Lucas Ferreira <lucasferreira059@gmail.com>
2026-05-01 17:00:35 -0300

    quase

---
+++ EstruturaBasica/estruturas.py
@@ -0,0 +1,23 @@
+#IF
+idade = 18
+
+if idade >= 18:
+    print("Acesso permitido")
+
+else:
+    print("Acesso negado")
+
+
+#FOR LOOP
+nomes = ["Lucas", "Carolina", "Rosineide"]
+
+for nome in nomes:
+    print(nome)
+
+
+#WHILE
+contador = 0
+
+while contador < 10:
+    print(contador)
+    contador += 1
